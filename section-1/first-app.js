067cbe0cb2eeb40138c7a9243a253c000064ab25
Robert Hieger <rhieger@gmail.com>
2019-09-12 08:02:22 -0400

    Lecture 3: Completed Installing Node.js and Creating Our First App.
    Variation on “Hello World!”

---
+++ section-1/first-app.js
@@ -0,0 +1,10 @@
+//  first-app.js
+
+// Section 1: Lecture 3
+
+// This very simple app displays a variant of the "Hello World!" cliche.
+
+// Robert Hieger
+// August 3, 2019
+
+console.log('\nHello from Node.js!\n');
