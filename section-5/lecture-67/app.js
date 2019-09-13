39d83b9b1142f2931a1a8c0b0d7af7f08eab8441
Robert Hieger <rhieger@gmail.com>
2019-09-13 12:48:57 -0400

    Lecture 67 Complete: Filtering Paths
    Files involved:
    
    app.js
    package-lock.json
    package.json
    /routes/admin.js
    /routes/shop.js

---
+++ section-5/lecture-67/app.js
@@ -0,0 +1,68 @@
+/*
+ ***************************************************************
+ *                                                             *
+ * Lecture 67: Filtering Paths                                 *
+ *                                                             *
+ * app.js                                                      *
+ *                                                             *
+ * Robert Hieger                                               *
+ * September 6, 2019                                           *
+ *                                                             *
+ * Picking up where the last lecture left off, this lecture    *
+ * explores filtering paths by setting a default app root      *
+ * to which the rest of the fully-formed URL will be appended. *
+ *                                                             *
+ ***************************************************************
+*/
+
+// Import necessary third party modules:
+
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+// Import custom modules:
+
+const adminRoutes = require('./routes/admin');
+
+const shopRoutes = require('./routes/shop');
+
+
+// Initialize Express app:
+
+const app = express();
+
+// Set request body parsing:
+
+app.use( bodyParser.urlencoded( {extended: false} ) );
+
+// Configure middleware:
+
+app.use('/admin', adminRoutes);
+
+// NOTE: Above, app.use is refactored by adding the URL 'filter' of '/admin'.
+// Therefore, any requests that fall within adminRoutes will automatically
+// prepend the '/admin' to whatever the rest of the URL request is.
+
+app.use(shopRoutes);
+
+// Catchall Middleware:
+
+app.use( (req, res, next) => {
+
+    res.status(404).send('<h1>Page not found!</h1>');
+
+}); // end app.use() catchall
+
+// NOTE: This middleware omits a path as the default IS '/' and
+// that should allow anything that comes after the site root.
+
+// Initialize an HTTP server using simplified Express.js syntax:
+
+app.listen(3000, () => {
+
+    // Log message to console confirming server operation:
+    
+    console.log('\nHTTP Server listening on port 3000...\n');
+
+});
