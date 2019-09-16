7a2c6b422d2e2575ce795bde42318743a43fc84a
Robert Hieger <rhieger@gmail.com>
2019-09-16 00:44:31 -0400

    Lecture 82 Complete: Converting HTML files to Pug.
    Files involved:
    
    app.js
    /views/404.pug

--- section-6/lecture-82/app.js
+++ section-6/lecture-82/app.js
@@ -76,7 +76,7 @@ app.use(shopRoutes);
 
 app.use( (req, res, next) => {
 
-    res.status(404).sendFile( path.join(__dirname, 'views', '404.html') );
+    res.status(404).render('404');
 
 }); // end app.use() catchall
 
