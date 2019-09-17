4e7a2fba12d45b92bfc9c18a242a45805b34dcc9
Robert Hieger <rhieger@gmail.com>
2019-09-16 23:15:33 -0400

    Lecture 84: Initialized project snapshot.

---
+++ section-6/lecture-84/routes/shop.js
@@ -0,0 +1,53 @@
+/*
+ ***************************************************************
+ *                                                             *
+ * shop.js                                                     *
+ *                                                             *
+ * Robert Hieger                                               *
+ * September 17, 2019                                          *
+ *                                                             *
+ * This module contains the routing logic for the Shop page    *
+ * which is comprised of the items on sale in the theoretical  *
+ * application now under development.                          *
+ *                                                             *
+ ***************************************************************
+*/
+
+// Import required core modules:
+
+const path = require('path');
+
+// Import required third party modules:
+
+const express = require('express');
+
+// Import required custom module:
+
+const rootDir = require('../util/path');
+
+// Import mock database contained in products array:
+
+const adminData = require('./admin');
+
+// Initialize Router service:
+
+const router = express.Router();
+
+// ROUTES:
+
+router.get('/', (req, res, next) => {
+
+    // Declare data source for products:
+
+    const products = adminData.products;
+
+    // Send response:
+
+    res.render( 'shop', { prods: products, docTitle: 'Shop' } );
+
+    
+}); // end router.get()
+
+// Export router:
+
+module.exports = router;
