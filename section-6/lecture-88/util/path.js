ca13fd0bce9b6c0299ce84df91c66db6bbd44187
Robert Hieger <rhieger@gmail.com>
2019-09-20 15:53:20 -0400

    Lecture 88: Initialized project snapshot.

---
+++ section-6/lecture-88/util/path.js
@@ -0,0 +1,23 @@
+/*
+ ***************************************************************
+ *                                                             *
+ * path.js                                                     *
+ *                                                             *
+ * Robert Hieger                                               *
+ * September 20, 2019                                          *
+ *                                                             *
+ * This module contains a helper function used to refine the   *
+ * navigation for this application.                            *
+ *                                                             *
+ ***************************************************************
+*/
+
+
+// Import required core modules:
+
+const path = require('path');
+
+// Construct a variable that helps construct a path to the
+// parent directory:
+
+module.exports = path.dirname(process.mainModule.filename);
