034b57a33be517ba4695f88a8fe55558fe38e2f5
Robert Hieger <rhieger@gmail.com>
2019-09-20 01:07:54 -0400

    Lecture 86: Registered and configured express-handlebars with Express.

--- section-6/lecture-86/app.js
+++ section-6/lecture-86/app.js
@@ -29,6 +29,8 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 
+const expressHbs = require('express-handlebars');   // express-handlebars engine
+
 // Import custom modules:
 
 const adminData = require('./routes/admin');
@@ -42,7 +44,11 @@ const app = express();
 
 // Set view engine for the application:
 
-app.set('view engine', 'pug');
+// Register express-handlebars as a templating engine usable by Express:
+
+app.engine('hbs', expressHbs());
+
+app.set('view engine', 'hbs');
 
 // Set path to templates:
 
