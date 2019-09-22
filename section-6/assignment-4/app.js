eaff30730f1e1fe74881c5682acc17c2a3f81fc1
Robert Hieger <rhieger@gmail.com>
2019-09-21 20:27:22 -0400

    Assignment 4: Created application file structure and app.js stub.

---
+++ section-6/assignment-4/app.js
@@ -0,0 +1,17 @@
+/*
+ ***************************************************************
+ *                                                             *
+ * app.js                                                      *
+ *                                                             *
+ * User List Application                                       *
+ *                                                             *
+ * Robert Hieger                                               *
+ * September 21, 2019                                          *
+ *                                                             *
+ * This module is the entry point to the User List app, and    *
+ * contains the Node.js server logic, as well as all critical  *
+ * imports and hooks to other parts of the application.        *
+ *                                                             *
+ ***************************************************************
+*/
+
