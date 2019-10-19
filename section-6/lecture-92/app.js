4c92dc0f56031931ee710f16e66ef3377a9d2127
Robert Hieger <rhieger@gmail.com>
2019-10-19 11:09:11 -0400

    Lecture 92: Created stub routes for app.

--- section-6/lecture-92/app.js
+++ section-6/lecture-92/app.js
@@ -6,6 +6,26 @@ const express = require('express');
 
 const app =  express();
 
+// ROUTES:
+
+app.get('/', (req, res, next)  =>  {
+
+    res.render(path);
+
+}); // end app.get('/')
+
+app.get('/users', (req, res, next)  =>  {
+
+    res.render(path);
+
+}); // end app.get('/users')
+
+app.post('/add-user', (req, res, next)  =>  {
+
+     res.redirect('/users');
+
+}); // end app.post('/add-user')
+
 // Set Listening port and start Node.js HTTP Server:
 
 app.listen(3000, () => {
