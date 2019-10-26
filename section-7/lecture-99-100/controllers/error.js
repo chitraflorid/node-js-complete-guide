8663b973a3b20570c0bf3c4181b4a74cd14e2e34
Robert Hieger <rhieger@gmail.com>
2019-10-26 12:14:19 -0400

    Lecture 99-100 Complete: Creating Model and Storing JSON to File.
    NOTE: Somehow accidentally combined content from Lecture 99 and 100, but result is the code at the end of Lecture 100.

---
+++ section-7/lecture-99-100/controllers/error.js
@@ -0,0 +1,26 @@
+/*
+ ***************************************************************
+ *                                                             *
+ * error.js                                                    *
+ *                                                             *
+ * Robert Hieger                                               *
+ * October 25, 2019                                            *
+ *                                                             *
+ * This module contains the controller to present a 404        *
+ * page error.                                                 *
+ *                                                             *
+ ***************************************************************
+*/
+
+exports.get404 = (req, res, next) => {
+
+    res.status(404).render('404',
+
+        {
+
+            pageTitle: 'Page Not Found',
+            path: '/*'
+
+        } );    // end res.status(404).render('404')
+
+};  // end exports.get404()
