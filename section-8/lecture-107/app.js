c6d64a27c53747132f143705beba807005b16858
Robert Hieger <rhieger@gmail.com>
2019-11-10 11:20:39 -0500

    Lecture 107: Initialized project snapshot.

---
+++ section-8/lecture-107/app.js
@@ -0,0 +1,88 @@
+/*
+ ***************************************************************
+ *                                                             *
+ * Lecture 107: Working on the Navigation                      *
+ *                                                             *
+ * app.js                                                      *
+ *                                                             *
+ * Robert Hieger                                               *
+ * Novmbeer 10, 2019                                           *
+ *                                                             *
+ * After adding additional views in the last lecture in order  *
+ * to structure the online shop, this lecture turns its        *
+ * attention to working on and making the navigation for the   *
+ * application more complete.                                  *
+ *                                                             *
+ ***************************************************************
+*/
+
+// Import necessary core modules:
+
+const path = require('path');
+
+// Import necessary third party modules:
+
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+// Import custom modules:
+
+const adminRoutes = require('./routes/admin');
+
+const shopRoutes = require('./routes/shop');
+
+const errorController = require('./controllers/error');
+
+
+// Initialize Express app:
+
+const app = express();
+
+// Set view engine for the application:
+
+app.set('view engine', 'ejs');
+
+// Set path to templates:
+
+app.set('views', 'views');
+
+// NOTE: '/views' is the default path for templates in Express.
+// This path is set here just to demonstrate how this is done.
+// The only time this would be used in production is if a different
+// path were chosen in which to store template files.
+
+// Set request body parsing:
+
+app.use( bodyParser.urlencoded( {extended: false} ) );
+
+// Set up static file serving from /public folder:
+
+app.use( express.static( path.join(__dirname, 'public') ) );
+
+// Configure middleware:
+
+app.use('/admin', adminRoutes);
+
+// NOTE: Above, app.use is refactored by adding the URL 'filter' of '/admin'.
+// Therefore, any requests that fall within adminRoutes will automatically
+// prepend the '/admin' to whatever the rest of the URL request is.
+
+app.use(shopRoutes);
+
+// Catchall Middleware:
+
+app.use(errorController.get404); // end app.use() catchall
+
+// NOTE: This middleware omits a path as the default IS '/' and
+// that should allow anything that comes after the site root.
+
+// Initialize an HTTP server using simplified Express.js syntax:
+
+app.listen(3000, () => {
+
+    // Log message to console confirming server operation:
+    
+    console.log('\nHTTP Server listening on port 3000...\n');
+
+});
---
+++ section-8/lecture-107/controllers/products.js
@@ -0,0 +1,81 @@
+/*
+ ***************************************************************
+ *                                                             *
+ * products.js                                                 *
+ *                                                             *
+ * Robert Hieger                                               *
+ * November 9, 2019                                            *
+ *                                                             *
+ * This module contains controllers with product-related       *
+ * logic, including listing or adding products to the          *
+ * products page.                                              *
+ *                                                             *
+ ***************************************************************
+*/
+
+// Import required custom modules:
+
+const Product = require('../models/product');
+
+// Add Product Route:
+
+exports.getAddProduct = (req, res, next) => {
+
+    // Send response:
+
+    res.render('admin/add-product',
+
+        {
+
+            pageTitle: 'Add Product',
+            path: '/admin/add-product',
+            formsCSS: true,
+            productCSS: true,
+            activeAddProduct: true
+
+        } );    // end res.render('add-product')
+
+};  // end exports.getAddProduct()
+
+// Add Product Logic:
+
+exports.postAddProduct = (req, res, next) => {
+
+    // Instantiate object of type Product:
+
+    const product = new Product(req.body.title);
+
+    // Save new product to products array:
+
+    product.save();
+
+    // Redirect to Landing Page.
+    
+    res.redirect('/');
+
+};  // end exports.postAddProduct()
+
+// Logic to get products for display in view:
+
+exports.getProducts = (req, res, next) => {
+
+    // Get all products for rendering:
+
+    Product.fetchAll( (products) =>  {
+
+        // Send response:
+
+        res.render( 'shop/product-list',
+
+        {
+            prods: products,
+            pageTitle: 'Shop', path: '/',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+
+        }); // end res.render('shop')
+
+    }); // end products()    
+  
+};  // end exports.getProducts()
