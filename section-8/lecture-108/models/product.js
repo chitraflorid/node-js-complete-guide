990914c2f52bb4fd012b32cd723776dc873397fd
Robert Hieger <rhieger@gmail.com>
2019-11-11 22:53:07 -0500

    Lecture 108 Complete: Registering the Routees
    NOTE: Highly suspected bug in routing /admin/add-product and /admin/product routes.

---
+++ section-8/lecture-108/models/product.js
@@ -0,0 +1,99 @@
+/*
+ ***************************************************************
+ *                                                             *
+ * product.js                                                  *
+ *                                                             *
+ * Robert Hieger                                               *
+ * November 10, 2019                                           *
+ *                                                             *
+ * This module contains a model for the data structure of a    *
+ * product that will be rendered on the Shop page.             *
+ *                                                             *
+ ***************************************************************
+*/
+ 
+// Import required core modules:
+ 
+const fs = require('fs');
+ 
+const path = require('path');
+
+const p = path.join(
+
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+
+);  // end const p
+
+// Helper Function:
+
+const getProductsFromFile = callBack =>  {
+
+    // Read in data file:
+
+    fs.readFile( p, (err, fileContent) => {
+
+        if (err)  {
+
+            // Return empty array:
+
+            return callBack([]);
+
+        }   // end if
+
+        // Return array of products data:
+
+        callBack(JSON.parse(fileContent));
+
+    }); // end fs.readFile(p)
+
+};  // end getProductsFromFile()
+ 
+// Declare Product class:
+ 
+module.exports = class Product {
+ 
+    constructor(t)  {
+ 
+        this.title = t;
+ 
+    }   // end constructor(title)
+ 
+    // METHODS:
+ 
+    save()  {
+ 
+        getProductsFromFile( products =>  {
+
+            // Push new product onto products array:
+
+            products.push(this);
+
+            // Write new product to file:
+
+            fs.writeFile( p, JSON.stringify(products), (err) => {
+
+                console.log(err);
+
+            });
+
+        });
+
+        fs.readFile(p, (err, fileContent) => {
+
+            // Declare variable to hold product data:
+
+            let products = [];
+
+        }); // end fs.readFile(p)
+ 
+    }   // end save()
+ 
+    static fetchAll(callBack)  {
+
+        getProductsFromFile(callBack);
+                            
+    }   // end fetchAll()
+ 
+};  // end Product()
