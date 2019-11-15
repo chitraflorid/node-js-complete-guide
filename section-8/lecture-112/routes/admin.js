50c0abae31be57c8a29030e16c62b921291f96fe
Robert Hieger <rhieger@gmail.com>
2019-11-15 12:21:36 -0500

    Lecture 112: Initialized project snapshot.

---
+++ section-8/lecture-112/routes/admin.js
@@ -0,0 +1,50 @@
+/*
+ ***************************************************************
+ *                                                             *
+ * admin.js                                                    *
+ *                                                             *
+ * Robert Hieger                                               *
+ * November 15, 2019                                           *
+ *                                                             *
+ * This module contains the routing logic for the Admin page   *
+ * that allows addition of products to the online store in     *
+ * this sample application.                                    *
+ *                                                             *
+ ***************************************************************
+*/
+
+
+// Import required core modules:
+
+const path = require('path');
+
+// Import required third party modules:
+
+const express = require('express');
+
+// Import custom module:
+
+const adminController = require('../controllers/admin');
+
+// Initialize Router service:
+
+const router = express.Router();
+
+// ROUTES:
+
+
+//  /admin/add-product => GET
+
+router.get('/add-product', adminController.getAddProduct);
+
+//  /admin/products => GET
+
+router.get('/products', adminController.getProducts);
+
+//  /admin/add-product => POST
+
+router.post('/add-product', adminController.postAddProduct);
+
+// Export router for use in other modules:
+
+module.exports = router;
