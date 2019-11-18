a46b34327705783e924fb4775ff30f66875a6fa3
Robert Hieger <rhieger@gmail.com>
2019-11-18 00:12:56 -0500

    Lecture 117: Initialized project snapshot.

---
+++ section-9/lecture-117/controllers/admin.js
@@ -0,0 +1,81 @@
+/*
+ ***************************************************************
+ *                                                             *
+ * admin.js                                                    *
+ *                                                             *
+ * Robert Hieger                                               *
+ * November 18, 2019                                           *
+ *                                                             *
+ * This module contains controllers that embody the logic      *
+ * necessary for maintaining the product base.                 *
+ *                                                             *
+ ***************************************************************
+*/
+
+// Import required custom modules:
+
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+
+    // Send response:
+
+    res.render('admin/add-product',
+
+        {
+
+            pageTitle: 'Add Product',
+            path: '/admin/add-product',
+            formsCSS: true,
+            productCSS: true,
+            activeAddProduct: true
+
+        } );    // end res.render('add-product')
+
+};  // end exports.getAddProduct()
+
+// Add Product Logic:
+
+exports.postAddProduct = (req, res, next) => {
+
+    // Elements to be saved to product:
+
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const description = req.body.description;
+    const price = req.body.price;
+
+    // Instantiate object of type Product:
+
+    const product = new Product(
+      title,
+      imageUrl,
+      description,
+      price
+    );
+
+    // Save new product to products array:
+
+    product.save();
+
+    // Redirect to Landing Page.
+    
+    res.redirect('/');
+
+};  // end exports.postAddProduct()
+
+exports.getProducts = (req, res, next) => {
+
+    Product.fetchAll(products => {
+
+      res.render('admin/products', {
+
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+
+      });   // end res.render('admin/products')
+
+    }); // end Product.fetchAll()
+
+  };    // end exports.getProducts()
---
+++ section-9/lecture-117/controllers/error.js
@@ -0,0 +1,26 @@
+/*
+ ***************************************************************
+ *                                                             *
+ * error.js                                                    *
+ *                                                             *
+ * Robert Hieger                                               *
+ * November 18, 2019                                           *
+ *                                                             *
+ * This module contains the controller to present a 404        *
+ * page error.                                                 *
+ *                                                             *
+ ***************************************************************
+*/
+
+exports.get404 = (req, res, next) => {
+
+    res.status(404).render('404',
+
+        {
+
+            pageTitle: 'Page Not Found',
+            path: '/404'
+
+        } );    // end res.status(404).render('404')
+
+};  // end exports.get404()
